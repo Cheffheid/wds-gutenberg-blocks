f235f85f0c4864b733d053a42d6aba0dc939bbe0
Jeffrey de Wit <jeffrey@webdevstudios.com>
2018-01-19 15:45:56 -0500

    Add a select element for background type

--- blocks/block-options/background-options.js
+++ blocks/block-options/background-options.js
@@ -14,6 +14,8 @@ const {
 	PanelColor,
 } = wp.components;
 
+const SelectControl = wp.blocks.InspectorControls.SelectControl;
+
 /**
  * Create an Inspector Controls wrapper Component
  */
@@ -24,14 +26,47 @@ export default class BackgroundOptions extends Component {
 	}
 
 	render() {
+		const backgroundType = this.props.attributes.backgroundType;
+
+		let colorPanel = <PanelColor title={ __( 'Background Color' ) } colorValue={ this.props.attributes.backgroundColor } initialOpen={ false }>
+							<ColorPalette
+								value={ this.props.attributes.backgroundColor }
+								onChange={ this.props.onChangeBackgroundColor }
+							/>
+						</PanelColor>;
+
+		// Don't show the colour panel if the background type is not "color".
+		if ( 'color' !== backgroundType ) {
+			colorPanel = '';
+			this.props.onChangeBackgroundColor( '' );
+		}
+
 		return (
 			<InspectorControls key="inspector">
-				<PanelColor title={ __( 'Background Color' ) } colorValue={ this.props.attributes.backgroundColor } initialOpen={ false }>
-					<ColorPalette
-						value={ this.props.attributes.backgroundColor }
-						onChange={ this.props.onChangeBackgroundColor }
-					/>
-				</PanelColor>
+
+				<SelectControl
+					key="background-type"
+					label={ __( 'Background' ) }
+					value={ this.props.attributes.backgroundType ? this.props.attributes.backgroundType : '' }
+					options={ [
+						{
+							label: __( 'None' ),
+							value: ''
+						},
+						{
+							label: __( 'Image' ),
+							value: 'image'
+						},
+						{
+							label: __( 'Color' ),
+							value: 'color'
+						},
+					] }
+					onChange={ this.props.onChangeBackgroundType }
+				/>
+
+				{colorPanel}
+
 				<PanelColor title={ __( 'Text Color' ) } colorValue={ this.props.attributes.textColor } initialOpen={ false }>
 					<ColorPalette
 						value={ this.props.attributes.textColor }
